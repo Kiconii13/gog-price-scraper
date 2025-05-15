48f453909a8d729f8a11ef3c89341ff19d952ae3
Kiconii13 <milan.markovic7572@gmail.com>
2025-05-15 02:38:12 +0200

    added requirements

--- scraper/__init__.py
+++ scraper/__init__.py
@@ -5,7 +5,7 @@ db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///your_database.db'  # ili tvoja baza
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:PbGOPztlsTtsMWaqQGzqdcFIbQJdhPpk@turntable.proxy.rlwy.net:26677/railway'  # ili tvoja baza
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
